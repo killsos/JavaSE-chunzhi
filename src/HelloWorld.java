d199a66994fb3530cc423d462d6924624b015d57
killsos <killsos.ql@gmail.com>
2018-06-05 00:01:16 +0800

    add

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,6 +2,8 @@
 public class HelloWorld {
 	public static void main(String[] args) {
 		Extend3 e3 = new Extend3();
+		System.out.println(e3.getClass());
+		System.out.println(e3.getClass().getName());
 		e3.say();
 	}
 	
