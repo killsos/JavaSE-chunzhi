8f406dd1d67ebab2f86064e55360dbc2aabac10f
killsos <killsos.ql@gmail.com>
2018-06-10 21:59:13 +0800

    add

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -7,6 +7,14 @@ public class HelloWorld {
 		TestPackage01SubClass tpsc = new TestPackage01SubClass();
 		
 		tpsc.name();
+		
+		String s1 = "123";
+		
+		String s2 = new String("123");
+		
+		System.out.println(s1.equals(s2));
+		
+		System.out.println(s1 == s2);
 	}
 	
 }
